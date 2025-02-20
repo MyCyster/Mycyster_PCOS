eabb23bc62f2d8a397bb370eaf88ca9806bfe7c5
Peace Edak <edak.peace@gmail.com>
2025-02-20 23:01:14 +0100

    new commit

--- src/components/LandingHome/PcosSolution.jsx
+++ src/components/LandingHome/PcosSolution.jsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import { solutionData } from "./HomeData"
 
 export const PcosSolution = () => {
+  const [shuffledData, setShuffledData] = useState(solutionData);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 800) { 
+        setShuffledData([solutionData[1], solutionData[0], solutionData[2], solutionData[3]]);
+      } else {
+        setShuffledData(solutionData);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <section className="px-6 sm:px-[2rem] lg:px-[5rem] py-1 md:py-4 text-center">
+    <section id="pcosSolution" className="px-6 sm:px-[2rem] lg:px-[5rem] py-1 md:py-4 text-center">
     {/* Title */}
     <h2 className="text-[21px] md:text-[25px] font-bold text-gray-900">
       Introducing MyCyster:
@@ -13,16 +31,13 @@ export const PcosSolution = () => {
 
     {/* Dynamic Feature Grid */}
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-6 mt-6 sm:mt-10">
-      {solutionData.map((feature, index) => (
+      {shuffledData.map((feature, index) => (
         <div
           key={index}
           className={`${feature.bgColor} rounded-lg p-6 text-left shadow`}
         >
           <div className="">
             <img src={feature.img} alt="" className={`${feature.textColor} w-[30px]`}/>
-            {/* <span className={`${feature.textColor} text-2xl`}>
-              {feature.icon}
-            </span> */}
             <h4 className={`text-lg font-bold mt-2 sm:mt-4 ${feature.textColor}`}>
               {feature.title}
             </h4>
