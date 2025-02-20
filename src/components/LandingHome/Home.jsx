cf52fb1900df16071de87bb24b72b3eb1ba20f6a
Peace Edak <edak.peace@gmail.com>
2025-02-20 17:40:07 +0100

    Landing Page commit

--- src/components/LandingHome/Home.jsx
+++ src/components/LandingHome/Home.jsx
@@ -1,9 +1,19 @@
+import { Footer } from "../Footer"
 import { Navbar } from "../Navbar"
+import { Hero } from "./Hero"
+import { LatestArticles } from "./LatestArticles"
+import { PcosSolution } from "./PcosSolution"
+import { SecondSection } from "./SecondSection"
 
 export const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Hero/>
+        <SecondSection/>
+        <PcosSolution/>
+        <LatestArticles/>
+        <Footer/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,23 @@
 import myCystLogo from '../assets/LandingHome/MyCysterLogo.png'
 import { useState } from 'react';
+import { Button } from './Button';
 
 export const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
   
     return (
-      <nav className="bg-white py-6 px-[2rem] lg:px-[5rem] flex justify-between items-center ">
+      <nav className="navbar relative z-50 py-6 px-[2rem] lg:px-[5rem] flex justify-between items-center -z-100 ">
         <div className="">
-            <img src={myCystLogo} alt="" className='w-[110px] md:text-[20px] font-bold text-[#057B7B]'/>
+            <img src={myCystLogo} alt="" className='w-[110px] md:w-[150px] font-bold text-[#057B7B]'/>
         </div>
         
-        <div className="hidden border border-[#057B7B] py-4 px-10 rounded-full text-[14px] lg:text-[16px] md:flex gap-[2rem] lg:gap-[4rem] xl:gap-[7rem] text-[#057B7B] bg-gradient-to-r from-white via-[#E3F4F4] to-[#A0CFCF] ">
+        <div className="hidden border border-[#057B7B] py-4 px-10 rounded-full text-[14px] lg:text-[16px] md:flex gap-[1.5rem] lg:gap-[4rem] xl:gap-[7rem] text-[#057B7B] bg-gradient-to-r from-white via-[#E3F4F4] to-[#A0CFCF] ">
           <a href="#features" className="">Features</a>
           <a href="#resources" className="">Resources</a>
           <a href="#community" className="">Join the Community</a>
         </div>
-        <button className="bg-[#057B7B] text-[#ffffff] rounded-full px-6 py-3 rounded-md hidden md:block text-[14px]">Get Started</button>
+        {/* <button className="bg-[#057B7B] text-[#ffffff] rounded-full px-6 py-3 rounded-md hidden md:block text-[14px]">Get Started</button> */}
+        <Button className="bg-[#ffffff] hidden md:block text-[#057B7B] ">Get Started</Button>
         
         <button onClick={() => setIsOpen(!isOpen)} className="md:hidden text-[32px] text-gray-700">☰</button>
         
